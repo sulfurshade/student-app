7a30fca96965d4b0b629832dfb52abde71878212
Joey Wolter <sulfurshade@gmail.com>
2017-09-29 10:57:57 -0400

    starter files

--- models.js
+++ models.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
 
-const blogPostSchema = mongoose.Schema({
-  author: {
+const studentSchema = mongoose.Schema({
+  name: {
     firstName: String,
     lastName: String
   },
-  title: {type: String, required: true},
-  content: {type: String},
-  created: {type: Date, default: Date.now}
+  instrument: {type: String, required: true},
+  username: {type: String, unique: true},
+  logs: [{notes:String, goals:String, date:Date, dueDate:Date}]
 });
 
 
-blogPostSchema.virtual('authorName').get(function() {
-  return `${this.author.firstName} ${this.author.lastName}`.trim();
-});
+// blogPostSchema.virtual('authorName').get(function() {
+//   return `${this.author.firstName} ${this.author.lastName}`.trim();
+// });
 
-blogPostSchema.methods.apiRepr = function() {
-  return {
-    id: this._id,
-    author: this.authorName,
-    content: this.content,
-    title: this.title,
-    created: this.created
-  };
-}
+// blogPostSchema.methods.apiRepr = function() {
+//   return {
+//     id: this._id,
+//     author: this.authorName,
+//     content: this.content,
+//     title: this.title,
+//     created: this.created
+//   };
+// }
 
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
