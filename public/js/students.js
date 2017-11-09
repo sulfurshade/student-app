678a36602222cd66591071c113118e8e0935eaf5
Joel Wallis Jucá <joelwallis@gmail.com>
2017-11-09 09:44:02 -0300

    Adds the API client as public/js/students.js

---
+++ public/js/students.js
@@ -0,0 +1,29 @@
+var Student = (function () {
+  function loadOne () {
+    return Promise.reject('Method not implemented')
+  }
+
+  function loadAll () {
+    return Promise.reject('Method not implemented')
+  }
+
+  function save () {
+    return Promise.reject('Method not implemented')
+  }
+
+  function update () {
+    return Promise.reject('Method not implemented')
+  }
+
+  function delete () {
+    return Promise.reject('Method not implemented')
+  }
+
+  return {
+    loadOne: loadOne,
+    loadAll: loadAll,
+    save: save,
+    update: update,
+    delete: delete
+  }
+})()
