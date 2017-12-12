cf8b75f9d2ee3619d11b749d9d3a476fdbf5d0d0
Joey Wolter <sulfurshade@gmail.com>
2017-12-12 09:25:13 -0500

    jwt error fix

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: '../.env'});
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
