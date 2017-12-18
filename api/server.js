26d455a54ccfab3e0470a8e1e1413edfc526d1a2
Joey Wolter <sulfurshade@gmail.com>
2017-12-18 14:54:31 -0500

    12182017

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '../.env'});
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
