3b426fc9e310bd65b4e9ff066d7abc330d419d44
Joel Wallis Jucá <joelwallis@gmail.com>
2018-01-25 11:12:11 -0300

    Removing remaining deprecated code

--- spa/js/global.js
+++
@@ -1,9 +0,0 @@
-const LOGOUT_BUTTON = "#logout-button";
-
-$(function() {
-	$(LOGOUT_BUTTON).click(function(e) {
-		e.preventDefault();
-		localStorage.removeItem("token");
-		window.location = "/index.html";
-	});
-});
