d5e5e5ad11643498bc2a63b383df310ac4d2cf02
Joey Wolter <sulfurshade@gmail.com>
2018-02-28 17:34:08 -0500

    3 tests passing

--- test/api/server.test.js
+++ test/api/server.test.js
@@ -7,6 +7,8 @@ const { seedDatabase, dropDatabase } = require('../lib/common')
 const { app, runServer, closeServer } = require('../../api/server')
 const { TEST_DATABASE_URL } = require('../../api/config')
 
+const User = require('../../api/models/User')
+
 chai.use(require('chai-http'))
 const expect = chai.expect
 
@@ -26,7 +28,7 @@ describe('Student Tracker', () => {
   afterEach(dropDatabase)
 
   describe('/users endpoint', () => {
-    it('lists all students', () => {
+    it('lists all users', () => {
       return chai.request(app)
         .get('/api/users')
         .then(res => {
@@ -37,25 +39,53 @@ describe('Student Tracker', () => {
         })
     })
 
-    it('creates a new student', () => {
+    it('creates a new user', () => {
+      const user = {
+        username: 'john-doe',
+        password: 'lololol',
+        firstName: 'John',
+        lastName: 'Doe'
+      }
+
       return chai.request(app)
         .post('/api/users')
-        .send({
-          foo: 'bar'
-          // username: 'john-doe',
-          // password: 'lololol',
-          // firstName: 'John',
-          // lastName: 'Doe'
-        })
-        .then(res => {
+        .send(user)
+        .then((res) => {
           expect(res).to.have.status(201)
-          // expect
+          expect(res).to.be.json
+          expect(res.body).to.be.an('object')
+
+          Object.keys(user).forEach((key) => {
+            if (['_id', 'password'].includes(key)) return
+            
+            expect(res.body[key]).to.be.ok
+            expect(res.body[key]).to.equal(user[key])
+          })
         })
     })
   })
 
   describe('/users/:id', () => {
-    it('shows a user')
+    it('shows a user', () => {
+      let user
+
+      User.findOne()
+        .then((doc) => (user = doc.toObject()))
+        .then(() => chai.request(app).get(`api/users/${user._id}`))
+        .then((res) => {
+          expect(res).to.have.status(200)
+          expect(res).to.be.json
+          expect(res.body).to.be.an('object')
+          expect(res.body.id).to.equal(user._id)
+
+          Object.keys(user).forEach((key) => {
+            if (['_id', 'password'].includes(key)) return
+
+            expect(res.body).to.have.key(key)
+            expect(res.body[key]).to.equal(user[key])
+          })
+        })
+    })
     it('updates a user')
     it('deletes a user')
   })
